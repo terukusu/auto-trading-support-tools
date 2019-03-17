f9b0752fbd2dadb6c6d2795173cf1ecb7df286ed
Teruhiko Kusunoki <teru.kusu@gmail.com>
2019-03-17 20:57:00 +0900

    MetaTraderのパスを設定ファイルに書く形から動的に探索する形に変更。

--- check_process.sh
+++ check_process.sh
@@ -3,23 +3,39 @@
 . `dirname $0`/common.sh
 
 function trd_check_process() {
-  name=$1
-  path=$2
+  name="$1"
+  path="$2"
 
-  terminal_win_path=`winepath -w "$path/terminal.exe" | sed -e 's/\\\\/\\\\\\\\/g'`
+  terminal_win_path=`winepath -w "$path" | sed -e 's/\\\\/\\\\\\\\/g'`
   count=`ps aux | grep -v grep | grep "$terminal_win_path" | wc -l`
 
   if [ "$count" -lt "1" ]; then
-    echo "MT4プロセスが動作していません: $name" | trd_send_to_line
+    echo "MetaTraderプロセスが動作していません: $name" | trd_send_to_line
   fi
 }
 
+# load MT4/5 list
+eval $(trd_gen_mt_list)
+
+num_target=$#
+
+if [ "$num_target" -le 0 ]; then
+    echo "Usage: `basename $0` <MetaTrader Name1> <MetaTrader Name2> ..." 1>&2
+    echo -e "\t<MetaTrader Name>: folder name of MetaTrader 4. (ex: "'"MetaTrader 4")'
+    exit 1
+fi
+
+target_names=("$@")
 i=0;
-while [ $i -lt $TRD_NUM_TERMINALS ]; do
-  name=`eval echo '$'TRD_NAME_$i`
-  path=`eval echo '$'TRD_MT4_PATH_$i`
+while [ $i -lt $num_target ]; do
+  target_name="${target_names[$i]}"
+  target_path="$(trd_find_terminal "$target_name")"
 
-  trd_check_process "$name" "$path"
+  if [ -n "$target_path" ]; then
+    trd_check_process "$target_name" "$target_path"
+  else
+    echo "チェック対象のMetaTraderのインストール場所が見つかりませ。: $target_name" | trd_send_to_line
+  fi
 
-  i=`expr $i + 1`
+  let i++
 done
