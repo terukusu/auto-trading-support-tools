5633c5260b32fac9e77c9180c47863bf3165e2bc
Teruhiko Kusunoki <teru.kusu@gmail.com>
2019-03-17 17:38:39 +0900

    WINEDIR以下からMT4/5を検索してリストアップする機能を追加。configファイル記載の手間を省くため。

--- common.sh
+++ common.sh
@@ -7,6 +7,10 @@ if [ ! -e $TRD_DATA_DIR ];then
     mkdir -p $TRD_DATA_DIR
 fi
 
+if [ -z "$WINEDIR" ];then
+    WINEDIR="$HOME/.wine"
+fi
+
 function trd_log() {
   echo "["`date "+%Y-%m-%d %H:%M:%S"`"] "$@
 }
@@ -38,4 +42,66 @@ function trd_send_to_line() {
         ]
       }'
   done
+}
+
+function trd_abs_path() {
+    target_dir=$(dirname "$1")
+    echo $(cd "$target_dir" && pwd)/$(basename "$1")
+}
+
+# scan wine drive and find terminal.exe. and output lines like below.
+#
+#    mt_dir[0]='/Users/teru/.wine/drive_c/Program Files/MetaTrader 4'
+#    mt_name[0]='METATRADER 4'
+#    mt_type[0]='MT4'
+#    mt_dir[1]....
+#    mt_name[1]....
+#    mt_type[1]....
+#
+# Evaluating like this, you can use them as valiable.
+#    eval $(trd_gen_mt_list)
+#
+function trd_gen_mt_list() {
+    i=0
+    find "$WINEDIR" -type f -name terminal.exe | while read line; do
+        line=$(trd_abs_path "$line")
+        mt_dir=$(dirname "$line")
+        mt_name=$(basename "$mt_dir" | tr '[a-z]' '[A-Z]')
+
+        if [ -d "$mt_dir/MQL4" ]; then
+            mt_type=MT4
+        elif [ -d "$mt_dir/MQL5" ]; then
+            mt_type=MT5
+        else
+            continue
+        fi
+
+        echo mt_dir[$i]="'$mt_dir'"
+        echo mt_name[$i]="'$mt_name'"
+        echo mt_type[$i]="'$mt_type'"
+
+        i=`expr $i + 1`
+    done
+
+    exit 0
+}
+
+# return absolute path of the terminal.exe
+# Returns the path of the termina.exe with a case-insensitive
+# prefix match between argument 1 and the folder name.
+#
+function trd_find_terminal() {
+    target_mt_name=$1
+
+    eval $(trd_gen_mt_list)
+
+    i=0
+    while [ $i -lt ${#mt_dir[@]} ]; do
+        match=`echo ${mt_name[$i]} | grep -iOE "^$target_mt_name"`
+        if [ -n "$match" ]; then
+            echo "${mt_dir[$i]}/terminal.exe"
+            break
+        fi
+        let i++
+    done
 }
