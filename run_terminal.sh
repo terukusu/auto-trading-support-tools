018ea95c41b4e6935755a7f5547a34026ebfcc14
terukusu <teru.kusu@gmail.com>
2019-03-21 17:11:34 +0900

    起動時にMetaTraderを自動起動する設定を追加。MT起動処理もそれに合わせて微調整。

--- run_terminal.sh
+++ run_terminal.sh
@@ -4,27 +4,42 @@
 
 target_name="$1"
 if [ -z "$target_name" ]; then
-  echo "Usage: `basename $0` <MetaTrader Name>" 1>&2
+  echo "Usage: `basename $0` <MetaTrader Name1> [<MetaTrader Name2> ...]" 1>&2
   echo -e "\t<MetaTrader Name>: folder name of MetaTrader 4. (ex: "'"MetaTrader 4")' 1>&2
   exit 1
 fi
 
-target_path="$(trd_find_terminal "$target_name")"
-if [ -z "$target_path" ]; then
-  echo "起動対象のMetaTraderのインストール場所が見つかりませ。: $target_name" 1>&2
-  exit 1
-fi
+# load MT4/5 list
+eval $(trd_gen_mt_list)
 
-work_dir="$(winepath -w "$(dirname "$target_path")")"
-wine_log="$TRD_DATA_DIR/wine_$(echo $target_name | trd_to_lower)"
+target_names=("$@")
+target_num=${#target_names[@]}
 
-if [ -z "$WINE" ]; then
-  WINE="$(which wine)"
-fi
+i=0;
+while [ "$i" -lt "$target_num" ]; do
+  target_name="${target_names[$i]}"
+  target_path="$(trd_find_terminal "$target_name")"
 
-echo -e "\n===== START `date +'%Y-%m-%d %H:%M:%S'` =====" >> "$wine_log"
+  if [ -z "$target_path" ]; then
+    echo "起動対象のMetaTraderのインストール場所が見つかりませ。起動をスキップします。: $target_name" 1>&2
+    let i++
+    continue
+  fi
 
-export WINEPREFIX WINEARCH WINEDEBUG
-nohup "$WINE" start /b /d "$work_dir" /unix "$target_path" &>> "$wine_log"
+ echo executing: "$target_path"
+
+  work_dir="$(winepath -w "$(dirname "$target_path")")"
+  wine_log="$TRD_DATA_DIR/wine_$(echo $target_name | trd_to_lower)"
+  
+  if [ -z "$WINE" ]; then
+    WINE="$(which wine)"
+  fi
+  
+  echo -e "\n===== START `date +'%Y-%m-%d %H:%M:%S'` =====" >> "$wine_log"
+  
+  nohup "$WINE" start /b /d "$work_dir" /unix "$target_path" &>> "$wine_log"
+
+  let i++
+done
 
 exit 0
