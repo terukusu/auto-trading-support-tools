f72e93d78529d1b22cdf39b8f530e4670c17bf49
Teruhiko Kusunoki <teru.kusu@gmail.com>
2019-03-23 20:26:55 +0900

    check_process.shのプロセス存在チェックを、common.shのpid検索関数を使うように修正。

--- check_process.sh
+++ check_process.sh
@@ -2,40 +2,23 @@
 
 . "$(cd "$(dirname $0)" && pwd)/common.sh"
 
+target_names=("$@")
+target_num=${#target_names[@]}
 
-function trd_check_process() {
-  name="$1"
-  path="$2"
-
-  terminal_win_path=`winepath -w "$path" | sed -e 's/\\\\/\\\\\\\\/g'`
-  count=`ps aux | grep -v grep | grep "$terminal_win_path" | wc -l`
-
-  if [ "$count" -lt "1" ]; then
-    echo "MetaTraderプロセスが動作していません: $name" | trd_send_to_line
-  fi
-}
-
-# load MT4/5 list
-eval $(trd_gen_mt_list)
-
-num_target=$#
-
-if [ "$num_target" -le 0 ]; then
+if [ "$target_num" -le 0 ]; then
   echo "Usage: `basename $0` <MetaTrader Name1> <MetaTrader Name2> ..." 1>&2
   echo -e "\t<MetaTrader Name>: folder name of MetaTrader 4. (ex: "'"MetaTrader 4")'
   exit 1
 fi
 
-target_names=("$@")
 i=0;
-while [ $i -lt $num_target ]; do
-  target_name="${target_names[$i]}"
-  target_path="$(trd_find_terminal "$target_name")"
+while [ "$i" -lt "$target_num" ]; do
+  target_name=${target_names[$i]}
 
-  if [ -n "$target_path" ]; then
-    trd_check_process "$target_name" "$target_path"
-  else
-    echo "チェック対象のMetaTraderのインストール場所が見つかりませ。: $target_name" | trd_send_to_line
+  target_pid=$(trd_find_pid "$target_name")
+
+  if [ -z "$target_pid" ]; then
+    echo "MetaTraderプロセスが動作していません: $target_name" | trd_send_to_line
   fi
 
   let i++
