b98bb428b543c4487115dd457a982010f3794656
Teruhiko Kusunoki <teru.kusu@gmail.com>
2019-04-09 21:23:22 +0900

    古いリングデータの切り捨て処理を追加

--- truncate_monitoring.sh
+++ truncate_monitoring.sh
@@ -7,29 +7,13 @@
 
 . "$(cd "$(dirname $0)" && pwd)/common.sh"
 
-target_names=("$@")
-target_num=${#target_names[@]}
+function do_check() {
+  local target_name="$1"
+  local target_fullname="$2"
 
-if [ "$target_num" -le 0 ]; then
-  echo "Usage: `basename $0` <MetaTrader Name1> <MetaTrader Name2> ..." 1>&2
-  echo -e "\t<MetaTrader Name>: folder name of MetaTrader 4. (ex: "'"MetaTrader 4")'
-  exit 1
-fi
+  local monitoring_file tmp_monitoring_file now from
 
-i=0;
-while [ "$i" -lt "$target_num" ]; do
-  target_name=${target_names[$i]}
-
-  target_index=$(atst_find_mt_index "$target_name")
-  target_path=${mt_home[$target_index]}
-  target_type=${mt_type[$target_index]}
-  if [ "$target_type" == "MT4" ]; then
-    target_mq_folder="MQL4"
-  else
-    target_mq_folder="MQL5"
-  fi
-
-  monitoring_file="$target_path/$target_mq_folder/Files/terminal_monitoring.csv"
+  monitoring_file=$(atst_get_monitoring_csv_path $target_name)
   tmp_monitoring_file=$(basename $monitoring_file .csv)_tmp.csv
 
   if [ -s "$monitoring_file" ]; then
@@ -38,6 +22,14 @@ while [ "$i" -lt "$target_num" ]; do
     cat "$monitoring_file" | awk -F, '{if ($1 > '$from') print}' > "$tmp_monitoring_file"
     mv "$tmp_monitoring_file" "$monitoring_file"
   fi
+}
 
-  let i++
-done
+target_names=("$@")
+
+if [ "$#" -le 0 ]; then
+  echo "Usage: `basename $0` <MetaTrader Name1> <MetaTrader Name2> ..." 1>&2
+  echo -e "\t<MetaTrader Name>: folder name of MetaTrader 4. (ex: "'"MetaTrader 4")'
+  exit 1
+fi
+
+atst_traverse_mt do_check "${target_names[@]}"
