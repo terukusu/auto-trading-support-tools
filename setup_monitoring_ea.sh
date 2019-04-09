762475174d7188a4cda9d8162e828fc89c5f0f89
Teruhiko Kusunoki <teru.kusu@gmail.com>
2019-04-09 21:01:43 +0900

    モニタリングEAをインストールする時にMT5フォルダにMT4のEAを入れてしまっていた不具合を修正。

--- setup_monitoring_ea.sh
+++ setup_monitoring_ea.sh
@@ -6,7 +6,12 @@
 for target_name in "$mt_name[@]"; do
   mql_folder_path=$(atst_get_mql_folder_path "$target_name")
 
-  echo installing TerminalMonitoring EA to $mql_folder_path
+  if [ -z "$(echo $mql_folder_path | grep -o MQL4)" ]; then
+    # not mt4. maybe mt5
+    continue
+  fi
+
+  echo Installing TerminalMonitoring EA to $mql_folder_path
 
   install -m 644 -D $ATST_HOME/MQL4/Experts/TerminalMonitoring.mq4 "$mql_folder_path/Experts/"
   install -m 644 -D $ATST_HOME/MQL4/Experts/TerminalMonitoring.ex4 "$mql_folder_path/Experts/"
