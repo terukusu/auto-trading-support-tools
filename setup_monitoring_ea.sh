a38d482fdd0e4dfeea0639df1bd712da2d46135d
Teruhiko Kusunoki <teru.kusu@gmail.com>
2019-04-11 23:36:23 +0900

    モニタリングデータ書き出し機能をMT5に対応。

--- setup_monitoring_ea.sh
+++ setup_monitoring_ea.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #
-# TODO: MT5対応
 #
 
 . "$(cd "$(dirname $0)" && pwd)/common.sh"
@@ -9,13 +8,13 @@
 for target_name in "${mt_name[@]}"; do
   mql_folder_path=$(atst_get_mql_folder_path "$target_name")
 
-  if [ -z "$(echo $mql_folder_path | grep -o MQL4)" ]; then
-    # not mt4. maybe mt5
-    continue
-  fi
-
   echo Installing TerminalMonitoring EA to $mql_folder_path
-
-  install -m 644 -D $ATST_HOME/MQL4/Experts/TerminalMonitoring.mq4 "$mql_folder_path/Experts/"
-  install -m 644 -D $ATST_HOME/MQL4/Experts/TerminalMonitoring.ex4 "$mql_folder_path/Experts/"
+  if [ -n "$(echo $mql_folder_path | grep -o MQL4)" ]; then
+    install -m 644 -D $ATST_HOME/MQL4/Experts/TerminalMonitoring.mq4 "$mql_folder_path/Experts"
+    install -m 644 -D $ATST_HOME/MQL4/Experts/TerminalMonitoring.ex4 "$mql_folder_path/Experts"
+  else
+    install -m 644 -D $ATST_HOME/MQL5/Experts/Advisors/TerminalMonitoring.mq5 "$mql_folder_path/Experts/Advisors"
+    install -m 644 -D $ATST_HOME/MQL5/Experts/Advisors/TerminalMonitoring.ex5 "$mql_folder_path/Experts/Advisors"
+  fi
+  echo "done"
 done
