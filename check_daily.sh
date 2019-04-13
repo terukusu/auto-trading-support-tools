c6ab089a2afdf59008ec2d50c58e566cca83cfeb
Teruhiko Kusunoki <teru.kusu@gmail.com>
2019-04-13 11:18:30 +0900

    アップグレード可能パッケージのカウントが１少なかった不具合を修正

--- check_daily.sh
+++ check_daily.sh
@@ -6,8 +6,8 @@
 echo "稼働OK！`uptime`" | atst_send_to_line
 
 # check upgradable package
-upgradable="`apt list --upgradable 2>/dev/null | grep / | cut -d'/' -f 1`"
-num_upgradable=`echo -n "$upgradable" | wc -l`
+upgradable=$(apt list --upgradable 2>/dev/null | grep / | cut -d'/' -f 1)
+num_upgradable=$(echo "$upgradable" | wc -l)
 
 if [ "$num_upgradable" -gt "0" ]; then
   echo -e "${num_upgradable}個のアップグレード可能なパッケージが有ります。\n$upgradable" |  atst_send_to_line
@@ -15,5 +15,5 @@ fi
 
 # check mt4 update
 if [ -d "$WINEPREFIX/drive_c/ProgramData/MetaQuotes/WebInstall/mt4clw" -o -d "$WINEPREFIX/drive_c/ProgramData/MetaQuotes/WebInstall/mt5clw" ]; then
-  echo "MetaTraderのアップデートが可能です" | atst_send_to_line
+  echo "MetaTraderのアップデートが可能です。MetaTraderを再起動してアップデートしてください。" | atst_send_to_line
 fi
