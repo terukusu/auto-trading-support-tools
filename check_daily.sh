031143058dd6ddeabb36a695c6daad787227fd35
Teruhiko Kusunoki <teru.kusu@gmail.com>
2019-04-14 14:37:55 +0900

    MT5のアプデを検知できない不具合を修正

--- check_daily.sh
+++ check_daily.sh
@@ -13,7 +13,23 @@ if [ "$num_upgradable" -gt "0" ]; then
   echo -e "${num_upgradable}個のアップグレード可能なパッケージが有ります。\n$upgradable" |  atst_send_to_line
 fi
 
-# check mt4 update
-if [ -d "$WINEPREFIX/drive_c/ProgramData/MetaQuotes/WebInstall/mt4clw" -o -d "$WINEPREFIX/drive_c/ProgramData/MetaQuotes/WebInstall/mt5clw" ]; then
-  echo "MetaTraderのアップデートが可能です。MetaTraderを再起動してアップデートしてください。" | atst_send_to_line
+# check mt update
+clwdir=(
+  "$WINEPREFIX/drive_c/users/$USER/Application Data/MetaQuotes/"
+  "$WINEPREFIX/drive_c/ProgramData/MetaQuotes"
+  "$WINEPREFIX/drive_c/Documents and Settings/All Users/Application Data/MetaQuotes"
+)
+
+update=$(for d in "${clwdir[@]}"; do find "$d" -name "mt?clw*" 2>/dev/null; done)
+
+if [ -n "$(echo "$update" | grep -oi mt4clw/terminal.exe)" ]; then
+  msg="MetaTrader 4のアップデートが可能です。"
+fi
+
+if [ -n "$(echo "$update" | grep -oi mt5)" -a -n "$(echo "$update" | grep -oi liveupdate)" ]; then
+  msg="${msg}MetaTrader 5のアップデートが可能です。"
+fi
+
+if [ -n "$msg" ]; then
+  echo "${msg}MetaTraderを再起動してアップデートしてください。" | atst_send_to_line
 fi
