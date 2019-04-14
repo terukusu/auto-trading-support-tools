84049b7dbac496ee9f1e4276c7d2e1ecbb925306
Teruhiko Kusunoki <teru.kusu@gmail.com>
2019-04-14 18:02:27 +0900

    引数の順番が分かりづらかったので、MT名を一番目の引数に変更。

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function print_usage_exit() {
-  echo "Usage $(basename $0) <script_name> <mt_names in 1 arg> [args for script...]"
+  echo "Usage $(basename $0) <mt_names in 1 arg> <script_name> [args for script...]"
   exit 1
 }
 
@@ -9,8 +9,8 @@ if [ $# -lt 2 ]; then
   print_usage_exit
 fi
 
-target_cmd="$1"
-eval "target_mt=$2"
+eval "target_mt=$1"
+target_cmd="$2"
 shift 2
 
 $target_cmd "$@" "${target_mt[@]}" >> $HOME/hoge
